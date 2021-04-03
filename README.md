f6161d8ac869c5f73c8edab30524a6d166bf6b3e
干净 <54979375+du5@users.noreply.github.com>
2021-04-03 23:41:55 +0800

    initial

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # IDWTU
 我不想升级
+
+
+## download 
+
+https://github.com/du5/idwtu/archive/refs/heads/main.zip
+
+
+## install 
+
+![image](./img/usage.png)
---
+++ content-script.js
@@ -0,0 +1,25 @@
+﻿var tid;
+function dele(find) {
+    var elementi = document.getElementsByClassName('netflix-sans-font-loaded');
+    if (elementi.length == 1 || ++find > 20) {
+        tid = setTimeout(dele, 100);
+        return;
+    }
+    for (let i = 0; i < elementi.length; i++) {
+        var elementj = elementi[i].parentElement;
+        if (elementj.id == 'appMountPoint') {
+            continue;
+        }
+        var elementk = elementj.querySelectorAll('[data-uia]');
+        for (let j = 0; j < elementk.length; j++) {
+            if (elementk[j].getAttribute('data-uia') == 'uma-action-PRICE_ACKNOWLEDGE') {
+                clearTimeout(tid);
+                elementj.remove();
+                document.body.classList.add("modal-open");
+                return;
+            }
+        }
+    }
+}
+
+dele(0);
